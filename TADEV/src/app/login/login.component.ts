06fd15a121d6493cd182e8b3e766451ac6949eed
Abhishek Kumar Misra <akumarmisra@dxc.com>
2020-01-21 20:06:39 +0530

    resolved conflicts

--- TADEV/src/app/login/login.component.ts
+++ TADEV/src/app/login/login.component.ts
@@ -47,32 +47,8 @@ export class LoginComponent implements OnInit {
   }
   refreshCaptch() {
     debugger;
-<<<<<<< HEAD
     this.captchaval = this._captchaService.generateCaptcha();
-=======
-    this.captchaval = Captcha();
->>>>>>> 822f18f190ab00337ffdd161430b380cf24a47af
     return false;
   }
 
 }
-<<<<<<< HEAD
-
-=======
-function Captcha() {
-  debugger
-  var alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
-  var i;
-  for (i = 0; i < 6; i++) {
-    var a = alpha[Math.floor(Math.random() * alpha.length)];
-    var b = alpha[Math.floor(Math.random() * alpha.length)];
-    var c = alpha[Math.floor(Math.random() * alpha.length)];
-    var d = alpha[Math.floor(Math.random() * alpha.length)];
-    var e = alpha[Math.floor(Math.random() * alpha.length)];
-    var f = alpha[Math.floor(Math.random() * alpha.length)];
-    var g = alpha[Math.floor(Math.random() * alpha.length)];
-  }
-  var code = a + ' ' + b + ' ' + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g;
-  return code;
-}
->>>>>>> 822f18f190ab00337ffdd161430b380cf24a47af
